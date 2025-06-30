e9e83632576b28bceefaf8cd3abc467141346653
kumsa Mergia <kumsamergia@gmail.com>
2025-06-30 16:52:42 +0300

    Implement FastAPI app to serve credit risk model with /predict endpoint

---
+++ src/api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from src.api.pydantic_models import CustomerInput, RiskPrediction
+import mlflow.pyfunc
+import pandas as pd
+
+app = FastAPI(title="Credit Risk Prediction API")
+
+# Load model from MLflow registry
+model = mlflow.pyfunc.load_model("models:/RandomForest/Production")
+
+@app.get("/")
+def root():
+    return {"message": "Credit Risk API is running."}
+
+@app.post("/predict", response_model=RiskPrediction)
+def predict_risk(customer: CustomerInput):
+    df = pd.DataFrame([customer.dict()])
+    prediction = model.predict(df)[0]
+    return {"risk_probability": float(prediction)}
